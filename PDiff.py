b3f4fe9c151e0dd4e85a12a73c7b7c220caf50e1
Ryan Quinn <rquinn@zappos.com>
2013-08-21 10:53:38 -0700

    Updated python

--- PDiff.py
+++ PDiff.py
@@ -1,14 +1,29 @@
 __author__ = 'rquinn'
+
 import os
+import json
 
+def createJSON():
+    records = []
+    record1 = {"name":"Bob", "email":"bob@email.com"}
+    records.append(record1)
+    record2 = {"name":"Bob2", "email":"bob2@email.com"}
+    records.append(record2)
+    return "Hello"
 
-if __name__ == "__main__":
-    #l = len(sys.argv)
+def diffIt():
 
     commandLine = 'perceptualdiff %s %s' % ('a.jpg', 'b.jpg')
     o = os.popen(commandLine)
     print o.readline()[0:4]
     o.close()
+
+if __name__ == "__main__":
+
+    print "Hello"
+    #l = len(sys.argv)
+
+
 
     # if l == 4:
     #     runTest(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
