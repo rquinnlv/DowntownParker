1e676ad8b7c8fc0cd065b892791625cb3584dbc7
Ryan Quinn <rquinn@zappos.com>
2013-08-21 10:06:27 -0700

    Updated with pdiff and script

--- PDiff.py
+++ PDiff.py
@@ -1,3 +1,16 @@
-__author__ = 'ryquinn'
+__author__ = 'rquinn'
+import os
 
 
+if __name__ == "__main__":
+    #l = len(sys.argv)
+
+    commandLine = 'perceptualdiff %s %s' % ('a.jpg', 'b.jpg')
+    o = os.popen(commandLine)
+    print o.readline()[0:4]
+    o.close()
+
+    # if l == 4:
+    #     runTest(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
+    # else:
+    #     print "Something"
